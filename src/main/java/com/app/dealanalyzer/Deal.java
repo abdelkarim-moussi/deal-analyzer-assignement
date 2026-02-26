7ec0b6f35bb16432b5ff2ea29492f4feac7de7ee
abdelkarim-moussi <abdelkarimmoussi73@gmail.com>
2026-02-26 22:10:30 +0000

    dockerize the application and the postgres db using dockerfile and docker-compose

--- src/main/java/com/app/dealanalyzer/Deal.java
+++ src/main/java/com/app/dealanalyzer/Deal.java
@@ -1,9 +1,6 @@
 package com.app.dealanalyzer;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,6 +16,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "deals")
 public class Deal {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
