b9f38b307ae1d2308b9e061f881be33af9a892f0
abdelkarim-moussi <abdelkarimmoussi73@gmail.com>
2026-02-26 02:23:09 +0000

    feat: implementing request deal service method

---
+++ src/main/java/com/app/dealanalyzer/DealService.java
@@ -0,0 +1,27 @@
+package com.app.dealanalyzer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DealService {
+
+    private final DealRepository repository;
+
+    public Deal requestDeal(DealRequest request){
+
+        if(repository.existsDealByFromCurrencyAndToCurrencyAndAmount(request.getFromCurrency(),
+                request.getToCurrency(),request.getDealAmount())){
+            throw new DealAlreadyExistException("There is Already A Deal With This Specifications");
+        }
+
+        Deal deal = Deal.builder()
+                .fromCurrency(request.getFromCurrency())
+                .toCurrency(request.getToCurrency())
+                .dealAmount(request.getDealAmount())
+                .build();
+
+        return repository.save(deal);
+    }
+}
