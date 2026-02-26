0687383db063548392822774754dd278c3dfa2d4
abdelkarim-moussi <abdelkarimmoussi73@gmail.com>
2026-02-26 09:29:44 +0000

    refactor: remove validation from Deal entity and put it in the Request

--- src/main/java/com/app/dealanalyzer/Deal.java
+++ src/main/java/com/app/dealanalyzer/Deal.java
@@ -7,9 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
@@ -17,19 +15,17 @@ import java.time.LocalDateTime;
 
 @Entity
 @Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Deal {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    @NotBlank
     private String fromCurrency;
-    @NotBlank
     private String toCurrency;
     @CreationTimestamp
     private LocalDateTime dealTimestamp;
-    @NotNull
-    @Min(value = 1)
     private BigDecimal dealAmount;
 }
